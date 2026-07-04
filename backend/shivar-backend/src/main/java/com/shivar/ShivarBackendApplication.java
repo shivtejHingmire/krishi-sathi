adcf4758ffdad1e33a035ca6681943ab33685a57
satyajitpawale-07 <satyajitpawale@gmail.com>
2026-07-04 20:27:34 +0530

    Created backend folder structure

---
+++ backend/shivar-backend/src/main/java/com/shivar/ShivarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shivar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShivarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShivarBackendApplication.class, args);
+    }
+
+}
---
+++ backend/shivar-backend/src/test/java/com/shivar/ShivarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShivarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
